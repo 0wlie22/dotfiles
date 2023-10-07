9afb96956bfbd788e9457fae65c3698f65bdcbfb
TAASEDAE <dari.sedova@gmail.com>
2023-10-07 13:06:39 +0300

    chore: add new plugins

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -48,4 +48,19 @@ return {
 			require("which-key").setup({})
 		end,
 	},
+	{
+		"sindrets/diffview.nvim",
+		cmd = "DiffviewOpen",
+		dependencies = { "nvim-web-devicons" },
+		opts = {
+			default_args = {
+				DiffviewOpen = { "-uno" },
+			},
+			hooks = {
+				diff_buff_read = function()
+					vim.opt_local.colorcolumn = ""
+				end,
+			},
+		},
+	},
 }
