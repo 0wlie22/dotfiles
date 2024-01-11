2cc5f5a359c71248a40c30b1449a24079b3ecf8d
0wlie22 <dari.sedova@gmail.com>
2024-01-11 16:28:48 +0200

    chore: refactor zsh

--- zsh/5-completions.zsh
+++ zsh/5-completions.zsh
@@ -31,8 +31,49 @@ if command -v terraform >/dev/null; then
     complete -o nospace -C $(which terraform) terraform
 fi
 
-# 1passowrd-cli completion
-if command -v op >/dev/null; then
-    eval "$(op completion zsh)"
-    compdef _op op
+# # 1passowrd-cli completion
+# if command -v op >/dev/null; then
+#     eval "$(op completion zsh)"
+#     compdef _op op
+# fi
+
+#compdef localstack
+
+_localstack_completion() {
+    local -a completions
+    local -a completions_with_descriptions
+    local -a response
+    (( ! $+commands[localstack] )) && return 1
+
+    response=("${(@f)$(env COMP_WORDS="${words[*]}" COMP_CWORD=$((CURRENT-1)) _LOCALSTACK_COMPLETE=zsh_complete localstack)}")
+
+    for type key descr in ${response}; do
+        if [[ "$type" == "plain" ]]; then
+            if [[ "$descr" == "_" ]]; then
+                completions+=("$key")
+            else
+                completions_with_descriptions+=("$key":"$descr")
+            fi
+        elif [[ "$type" == "dir" ]]; then
+            _path_files -/
+        elif [[ "$type" == "file" ]]; then
+            _path_files -f
+        fi
+    done
+
+    if [ -n "$completions_with_descriptions" ]; then
+        _describe -V unsorted completions_with_descriptions -U
+    fi
+
+    if [ -n "$completions" ]; then
+        compadd -U -V unsorted -a completions
+    fi
+}
+
+if [[ $zsh_eval_context[-1] == loadautofunc ]]; then
+    # autoload from fpath, call function directly
+    _localstack_completion "$@"
+else
+    # eval/source/. command, register function for later
+    compdef _localstack_completion localstack
 fi
