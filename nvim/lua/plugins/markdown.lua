f9378129e2838623e81433a0d35e56d5f669c031
0wlie22 <dari.sedova@gmail.com>
2026-06-08 12:27:27 +0300

    fix: update markdown preview config

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,40 +1,42 @@
 return {
-    {
-        "iamcco/markdown-preview.nvim",
-        ft = { "markdown" },
-        build = function()
-            vim.fn["mkdp#util#install"]()
-        end,
-    },
-    {
-        "kiran94/edit-markdown-table.nvim",
-        config = true,
-        dependencies = { "nvim-treesitter/nvim-treesitter" },
-        cmd = "EditMarkdownTable",
-    },
-    {
-        "MeanderingProgrammer/render-markdown.nvim",
-        ft = "markdown",
-        dependencies = {
-            -- "3rd/image.nvim",
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-tree/nvim-web-devicons",
-        },
-        opts = {
-            mkdp_port = 8000,
-        },
-    },
-    -- {
-    -- 	"3rd/image.nvim",
-    -- 	dependencies = {
-    -- 		"leafo/magick",
-    -- 	},
-    -- 	opts = {
-    -- 		integrations = {
-    -- 			markdown = {
-    -- 				only_render_image_at_cursor = true,
-    -- 			},
-    -- 		},
-    -- 	},
-    -- },
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+	{
+		"kiran94/edit-markdown-table.nvim",
+		config = true,
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		cmd = "EditMarkdownTable",
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		ft = "markdown",
+		dependencies = {
+			-- "3rd/image.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			mkdp_port = 8000,
+		},
+	},
+	-- {
+	-- 	"3rd/image.nvim",
+	-- 	dependencies = {
+	-- 		"leafo/magick",
+	-- 	},
+	-- 	opts = {
+	-- 		integrations = {
+	-- 			markdown = {
+	-- 				only_render_image_at_cursor = true,
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
 }
